29b4e1208fda075a4ac9b1d919a444c5b529fc6f
hbp_01 <hector.bolsa@outlook.com>
2018-12-10 02:15:01 +0000

    Initial models and views to test basic CRUD functionalities. Migrations.

--- SRV-http/bceln/core/models.py
+++ SRV-http/bceln/core/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+from django.conf import settings
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    birth_date = models.DateField(null=True, blank=True)
+    
+    def __str__(self):
+        return self.get_full_name()
+    
+
+class Member(models.Model):
+    firstname = models.CharField(max_length=40)
+    lastname = models.CharField(max_length=40)
+    friendname = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    file = models.FileField(upload_to='uploads/', blank=True) # File is automatically uploaded to MEDIA_ROOT/uploads/
+    def __str__(self):
+        return self.firstname + " " + self.lastname
+    
+    
--- SRV-http/bceln/core/views.py
+++ SRV-http/bceln/core/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.conf import settings
+from core.models import Member, User
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required
+def index(request):
+    members = Member.objects.all()
+    context = {'members': members}
+    return render(request, 'core/index.html', context)
+
+def create(request):
+    if request.method == "POST":
+        member = Member(
+            firstname=request.POST['firstname'],
+            lastname=request.POST['lastname'],
+            friendname_id=request.POST['friendname'],
+            # Adding the get method ensures that if variable is empty, an empty value is assigned to 'file'
+            file = request.FILES.get('file', ''),
+            )
+        member.save()
+        return redirect('index')
+    else:
+        # Send user to the empty form along with the list of users to populate the drop-down control
+        friendslist = User.objects.all()
+        context = {'friendslist': friendslist}
+        return render(request, 'core/create.html', context)
+
+def edit(request, id):
+    members = Member.objects.get(id=id)
+    friendslist = User.objects.all()
+    context = {'members': members, 'friendslist': friendslist}
+    return render(request, 'core/edit.html', context)
+
+def update(request, id):
+    member = Member.objects.get(id=id)
+    member.firstname = request.POST['firstname']
+    member.lastname = request.POST['lastname']
+    member.friendname_id = request.POST['friendname']
+    # Adding the get method ensures that if variable is empty, an empty value is assigned to 'file'
+    member.file = request.FILES.get('file', '')
+    member.save()
+    return redirect('index')
+
+def delete(request, id):
+    member = Member.objects.get(id=id)
+    member.delete()
+    return redirect('index')
+    
